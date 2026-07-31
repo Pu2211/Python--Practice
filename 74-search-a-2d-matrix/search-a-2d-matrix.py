98050b50ee025580309553ee8060baffcc29ab00
Pushkar Chaudhari <134547593+Pu2211@users.noreply.github.com>
2026-07-31 18:35:26 +0530

    Time: 3 ms (5.18%) | Memory: 12.7 MB (31.47%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or not matrix[0]:
+            return False
+
+        m, n = len(matrix), len(matrix[0])
+        lo, hi = 0, m * n - 1
+
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            row, col = divmod(mid, n)
+            val = matrix[row][col]
+            if val == target:
+                return True
+            elif val < target:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
+        return False
